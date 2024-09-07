f7e215f17b2e5d516c2d5103cf28f5308731f792
dr-alberto <delrio.alberto@protonmail.com>
2024-09-07 13:27:50 +0200

    test: Update test command to build playwright service before run

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 docker compose -f docker-compose.test.yml up --build -d postgres backend frontend
 
+docker compose -f docker-compose.test.yml build --no-cache playwright
+
 docker compose -f docker-compose.test.yml run --rm playwright
 
 docker compose -f docker-compose.test.yml down
